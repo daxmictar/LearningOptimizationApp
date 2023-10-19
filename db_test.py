e10317e885ddc4d5886e4fe0733f3a6f3fb43958
Josh K <kvarn001@csusm.edu>
2023-10-19 16:34:55 -0700

    new functions and improved commenting in db_test.py
    wrote get_tags() and get_matching_video(), need to test them more

--- db_test.py
+++ db_test.py
@@ -1,23 +1,29 @@
 import sqlite3
 from db_con import get_db
 from tools.logging import logger
+from functools import reduce
 
-#drop and recreate movies table in local_data_base
+"""
+Name:       refresh_db
+Purpose:    drop and recreate the movies table with default values
+Parameter:  none
+Return:     none
+"""
 def refresh_db():
     db = get_db()
     cur = db.cursor()
 
-    #delete movies table ahead of (re)creation
+    #if movies table exists, drop it before (re)creation
     cur.execute("DROP TABLE IF EXISTS movies")
 
     #create movies table
+    cur.execute("CREATE TABLE movies (filename TEXT PRIMARY KEY, tags TEXT, watched INTEGER)")
     """
     key for attribute 'watched':
          -1 : watched with attention
           0 : unwatched (initial default)
         >=1 : watched partially or without attention, where value equals quantity of failures to watch with attention
     """
-    cur.execute("CREATE TABLE movies (filename TEXT PRIMARY KEY, tags TEXT, watched INTEGER)")
 
     #insert rows descriptive of movies located in static directory
     cur.execute("INSERT INTO movies VALUES ('movie.mp4', 'Trees Bus Short', 0)")
@@ -28,7 +34,12 @@ def refresh_db():
     db.commit()
     db.close()
 
-#in local_data_base, sets watched attribute to -1 of the video that has filename matching passed string
+"""
+Name:       set_watched
+Purpose:    set the watched flag of the video with passed filename to -1
+Parameter:  string representing video filename
+Return:     none
+"""
 def set_watched(movie):
     db = get_db()
     cur = db.cursor()
@@ -36,10 +47,9 @@ def set_watched(movie):
     #if watched flag is not already set on video with passed string as filename...
     cur.execute("SELECT watched FROM movies WHERE filename=?", (movie,))
     if cur.fetchone()!=(-1,):
-        #debug output
         logger.debug("Setting watched flag for " + movie)
 
-        #set watched flag of video with passed string as name
+        #set watched flag of video with passed filename to -1
         cur.execute("UPDATE movies SET watched=-1 WHERE filename=?", (movie,))
 
     db.commit()
@@ -47,24 +57,29 @@ def set_watched(movie):
     #debug messages
     logger.debug("Unwatched videos:")
     cur.execute("SELECT filename FROM movies WHERE watched=0")
-    logger.debug(cur.fetchall())
+    logger.debug(str(cur.fetchall()))
     logger.debug("Watched videos:")
     cur.execute("SELECT filename FROM movies WHERE watched=-1")
-    logger.debug(cur.fetchall())
+    logger.debug(str(cur.fetchall()))
 
     db.close()
 
-#returns the filename of a random unwatched movie, or returns passed string (same video) if no unwatched videos remain
+"""
+Name:       get_unwatched
+Purpose:    get the filename of a random unwatched video
+Parameter:  string representing video filename
+Return:     string representing video filename
+"""
 def get_unwatched(previous_video):
     db = get_db()
     cur = db.cursor()
 
-    #if there are any more unwatched videos, randomly select one and return its filename
+    #if there are any more unwatched videos, set next_video to the filename of a random unwatched video and return it
     cur.execute("SELECT COUNT(*) FROM movies WHERE watched=0")
     if cur.fetchone()[0]>0:
-        # !!!ALERT!!! remember random() will be too expensive here when number of movies is large
         cur.execute("SELECT filename FROM movies WHERE watched=0 ORDER BY random()") 
         next_video = cur.fetchone()[0]
+        db.close()
 
         logger.debug("Randomly selecting an unwatched video.")
 
@@ -72,25 +87,52 @@ def get_unwatched(previous_video):
     
     #if no more unwatched videos, return the passed string (previous video filename)
     else:
+        db.close()
         logger.debug("No unwatched videos remain. Replaying previous.")
         return previous_video
     
-#lookup db entry for video with passed string as filename, return its tags as a list of strings
+"""
+Name:       get_tags
+Purpose:    get list of tags for video with passed filename
+Parameter:  string representing video filename
+Return:     list of strings representing tags
+"""
 def get_tags(movie):
     db = get_db()
     cur = db.cursor()
 
     cur.execute("SELECT tags FROM movies WHERE filename=?", (movie,))
-    #fetch result of query and, using whitespace as delimiters, split it into a list
+    #split the result of the above query (string delimited by spaces) into a list of strings
     tag_list = (cur.fetchone()[0]).split()
 
     db.close()
 
     return tag_list
 
-#return list of filenames for videos which match all tags in passed list
+"""
+Name:       get_matching_videos
+Purpose:    get a list of video filenames which match all of the tags in the passed list of tags
+Parameter:  list of strings representing tags
+Return:     list of strings representing video filenames
+"""
 def get_matching_videos(tag_list):
     db = get_db()
     cur = db.cursor()
-
     
+    #initiate list of lists, where each list contains the results of a query for one tag
+    superlist = []
+    
+    #iterate through passed list
+    for tag in tag_list:
+        #query for list of filenames for videos which have the current tag
+        cur.execute("SELECT filename FROM movies WHERE (INSTR(tags, ?))>0", (tag,))
+        
+        #append list from above query to superlist
+        superlist.append(cur.fetchall())
+        
+    db.close()
+    
+    #create new list which contains only common elements (filenames) between all lists in superlist
+    match_list = list(reduce(lambda i, j: i & j, (set(x) for x in superlist)))
+    
+    return match_list
