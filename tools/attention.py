6312dfb2553904eaecabec47b3a706e3634baeeb
Dax Taraleskof <daxmtaraleskof@protonmail.com>
2023-12-07 14:35:40 -0800

    Added imitation setter and getter
    Functions for setting a global var in attention.py to simulate attention based on buttons pressed in the video survey after the main video.

--- tools/attention.py
+++ tools/attention.py
@@ -1,5 +1,4 @@
 """
-
 Attention API for collecting attention data and handling its state
 
 Priority Order for API calls:
@@ -23,6 +22,48 @@ from em_st_artifacts.utils.lib_settings import MathLibSetting, ArtifactDetectSet
     MentalAndSpectralSetting
 
 from tools.logging import logger
+
+
+"""
+Code simulation Section
+"""
+
+paid_attention: float = 0.00
+
+def set_paid_attention(new_attention_val: float, log=False):
+    global paid_attention
+
+    logger.debug(f"New attention value: {new_attention_val}") if log else None
+
+    paid_attention = new_attention_val
+
+def get_paid_attention(log=False):
+    global paid_attention
+    """ 
+        Will update based on user interaction with a button on the JS side.
+    """
+    if log:
+        debug_msg = ""
+        match (paid_attention):
+            case 0.00: 
+                debug_msg = "User paid no attention"
+            case 0.20:
+                debug_msg = "User paid little attention"
+            case 0.40:
+                debug_msg = "User paid some attention"
+            case 0.60:
+                debug_msg = "User paid moderate attention"
+            case 0.80:
+                debug_msg = "User paid significant attention"
+            case 1.00:
+                debug_msg = "User paid full attention"
+            case _:
+                debug_msg = "Bad value"
+
+        logger.debug(debug_msg) 
+
+    return paid_attention
+
 
 # calibration settings for EmotionalMath object from sample.py
 CALIBRATION_LENGTH = 8
