18ce7467d219842ed70fd01a894780b3c439317d
Dax Taraleskof <daxmtaraleskof@protonmail.com>
2023-12-10 15:30:25 -0800

    bugfix
    changed log message

--- open_calls/most_watched_tags.py
+++ open_calls/most_watched_tags.py
@@ -1,9 +1,11 @@
 from tools.database.db_lib import get_top_x_tags
 from tools.logging import logger
 
+TOP_X_TAGS = 5
+
 def handle_request():
-    logger.debug("Checking Headband Connection")
+    logger.debug(f"Getting top {TOP_X_TAGS} watched tags from database")
    
-    tags = get_top_x_tags(5)
+    tags = get_top_x_tags(TOP_X_TAGS)
     
     return tags
